0e3be188e4a747deb5affc8e265eb9521a7af91a
codepushed <mehrashubham216@gmail.com>
2024-09-06 00:00:44 +0530

    profile - user profile details

--- components/Sidebar/index.jsx
+++ components/Sidebar/index.jsx
@@ -1,29 +1,27 @@
 import React from "react";
-import Divider from '@mui/material/Divider';
+
 
 const Sidebar = () => {
-    return(
-<div className="sidebarContainer">
-<div className="sidebarList sidebarListActive">
-    <img src="/assets/icons/profile_booking.png" alt="profile" />
-    <p>Profile</p>
+  return (
+    <div className="sidebarContainer">
+      <div className="sidebarList sidebarListActive">
+        <img src="/assets/icons/profile_booking.png" alt="profile" />
+        <p>Profile</p>
 
-    {/* <Divider /> */}
-    
-</div>
+        <div className="profileSidebarDivider"></div>
+      </div>
 
-<div className="sidebarList">
-    <img src="/assets/icons/mybookings.png" alt="mybookings" />
-    <p>My bookings</p>
-</div>
+      <div className="sidebarList">
+        <img src="/assets/icons/mybookings.png" alt="mybookings" />
+        <p>My bookings</p>
+      </div>
 
-
-<div className="sidebarList">
-    <img src="/assets/icons/logout.png" alt="logout" />
-    <p>Logout</p>
-</div>
-</div>
-    );
-}
+      <div className="sidebarList">
+        <img src="/assets/icons/logout.png" alt="logout" />
+        <p>Logout</p>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
