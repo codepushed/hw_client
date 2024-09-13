1af127f022ba994babd7da72d304c794f7215bb3
codepushed <mehrashubham216@gmail.com>
2024-09-13 16:46:36 +0530

    contact fix

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { isMobile } from "react-device-detect";
 
 import Header from "../../components/Header";
 
@@ -13,15 +14,20 @@ const Contact = () => {
         <div className="contactUs">
           <div className="contactUsMaps">
             <h2 className="contactUsHeading">Our office</h2>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3579.5756936462403!2d78.19696267565816!3d26.210477589915502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3976c400e460862d%3A0x57150911ded8276!2sSilver%20Estate!5e0!3m2!1sen!2sin!4v1726217658496!5m2!1sen!2sin"
-              width="600"
-              height="450"
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-              style={{ border: "none", borderRadius: "16px" }}
-            ></iframe>
+            <div className="contactusIframe">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3579.5756936462403!2d78.19696267565816!3d26.210477589915502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3976c400e460862d%3A0x57150911ded8276!2sSilver%20Estate!5e0!3m2!1sen!2sin!4v1726217658496!5m2!1sen!2sin"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+                style={{
+                  border: "none",
+                  borderRadius: "16px",
+                  height: "100%",
+                  width: "100%",
+                }}
+              ></iframe>
+            </div>
 
             <p>
               656X+5RM, University Rd, Thatipur, Gwalior, Madhya Pradesh 474011
