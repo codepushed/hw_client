f0fe1bc818f11cc30a934c78e12ff48d83951611
codepushed <mehrashubham216@gmail.com>
2024-10-04 14:39:46 +0530

    otp model

---
+++ components/Modal/OTP.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+
+const OTP = ({ isOpen, handleClose }) => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    let inputValue = e.target.value;
+
+    inputValue = inputValue.slice(0, 1);
+
+    inputValue = inputValue.replace(/[^0-9]/g, "");
+
+    setValue(inputValue);
+  };
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 500,
+    bgcolor: "background.paper",
+    outline: "none",
+    borderRadius: "16px",
+    marginTop: "10px",
+  };
+
+  return (
+    <div>
+      <Modal
+        open={true}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className="otpModalContainer">
+            <h2>Enter OTP</h2>
+            <p className="otpModalSubheading">
+              Enter the OTP sent to your phone to confirm job completition
+            </p>
+
+            <div className="otpModalInputs">
+              <input type="number" onChange={(e) => handleChange(e)} />
+              <input type="number" onChange={(e) => handleChange(e)} />
+              <input type="number" onChange={(e) => handleChange(e)} />
+              <input
+                type="number"
+                value={value}
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+          </div>
+
+          <div className="markasCompleteBtn otpBtn">
+            <button className="jobCardAcceptBtn">Submit</button>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default OTP;
--- pages/professional/dashboard.jsx
+++ pages/professional/dashboard.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ToggleWidget from "../../components/ToggleWidget";
 import Header from "../../components/Header";
 import ServiceDetailsModel from "../../components/Modal/ServiceDetailsModel";
+import OTP from "../../components/Modal/OTP";
 
 const Dashboard = () => {
   return (
@@ -12,6 +13,7 @@ const Dashboard = () => {
       <ToggleWidget />
 
       <ServiceDetailsModel />
+      <OTP />
     </div>
   );
 };
