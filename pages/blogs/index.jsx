8e42b7eb5ca0c70bf733dcebba32ff136927aadc
emmamyersCoder <myersemma450@gmail.com>
2024-10-07 13:58:54 +0530

    new blogs updated - content only

--- pages/blogs/index.jsx
+++ pages/blogs/index.jsx
@@ -4,20 +4,21 @@ import BlogListing from "../../Containers/Blog/BlogListing";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { BlogSeo } from "../../Seo/blog";
+import { blogs } from "../../Static/blogs";
 
-const BlogList = ({ isHeader }) => {
+const BlogList = ({ isHeader, blog }) => {
   return (
     <>
       <Header />
       <BlogSeo />
-      <BlogListing isHeader={isHeader} />
+      <BlogListing isHeader={isHeader} blogs={blog} />
       <Footer />
     </>
   );
 };
 
 BlogList.getInitialProps = async (ctx) => {
-  return { isHeader: true };
+  return { isHeader: true, blog: blogs };
 };
 
 export default BlogList;
