cfdfcc83190dde39700a742ae1dbf221408e5c67
codepushed <mehrashubham216@gmail.com>
2024-10-18 18:33:03 +0530

    service bentos

--- components/Section/Services.jsx
+++ components/Section/Services.jsx
@@ -2,87 +2,82 @@ import React from "react";
 
 const Services = () => {
   return (
-    <div className="servicesOfferedSection">
-      <h1>Services we offer</h1>
+    <div className="landingServicesContainer">
+      <h1>Services</h1>
+      <p>Services that fulfills your needs, trusted experts for every need</p>
+      <p>anytime, anywhere. Convenience and quality just a click away</p>
 
-      <div className="servicesOfferedSectionContainer">
-        <div className="servicesOfferedSectionContent">
-          <div className="servicesOfferedSectionContentImg">
-            <img
-              src="/assets/a women in a kitchen busy tying up a small sized t.webp"
-              alt="services"
-            />
-          </div>
-          <p className="servicesOfferedSectionContentText">Kitchen Cleaning</p>
-        </div>
+      <div className="landingServicesBentosContainer">
+        <div className="bentosRowContainer">
+          <div className="bentosRowOne">
+            <div className="landingServicesBentos">
+              <div className="landingServiceBentosImg">
+                <img src="/assets/taprepair.png" alt="taprepair" />
+              </div>
+              <div className="landingServicesTags">
+                <h1>Tap leakage fix</h1>
+              </div>
+            </div>
 
-        <div className="servicesOfferedSectionContent">
-          <div className="servicesOfferedSectionContentImg">
-            <img
-              src="/assets/man in workerdress painting a wall with ladder.webp"
-              alt="services"
-            />
-          </div>
-          <p className="servicesOfferedSectionContentText">
-            Full home painting
-          </p>
-        </div>
-
-        <div className="servicesOfferedSectionContent">
-          <div className="servicesOfferedSectionContentImg">
-            <img
-              src="/assets/photo of air conditioner on the wall.webp"
-              alt="services"
-            />
-          </div>
-          <p className="servicesOfferedSectionContentText">
-            Ac & repair service
-          </p>
-        </div>
-
-        <div className="servicesOfferedSectionContent">
-          <div className="servicesOfferedSectionContentImg">
-            <img
-              src="/assets/A female stylist dressed as a chola or pachuca.webp"
-              alt="services"
-            />
-          </div>
-          <p className="servicesOfferedSectionContentText">Salon servics</p>
-        </div>
-      </div>
-
-      <div
-        className="servicesOfferedSectionContainer"
-        style={{ marginTop: "80px" }}
-      >
-        <div className="servicesOfferedSectionContent">
-          <div className="servicesOfferedSectionContentImg">
-            <img
-              src="/assets/Man removing asbestos in a suit and respirator.webp"
-              alt="services"
-            />
-          </div>
-          <p className="servicesOfferedSectionContentText">
-            Cleaning & pest control
-          </p>
-        </div>
-
-        <div className="servicesOfferedSectionContent">
-          <div className="servicesOfferedSectionContentImg">
-            <img
-              src="/assets/Purple color plumber fixing water leak.webp"
-              alt="services"
-            />
+            <div className="landingServicesBentosThree">
+              <div className="landingServiceBentosImg">
+                <img src="/assets/switchboard.png" alt="taprepair" />
+              </div>
+              <div className="landingServicesTags">
+                <h1>Electrical switch board setup</h1>
+              </div>
+            </div>
           </div>
 
-          <p className="servicesOfferedSectionContentText">
-            Electrician, Plumber & Carpenter
-          </p>
-        </div>
-      </div>
+          <div className="bentosRowOne">
+            <div className="landingServicesBentosTwo">
+              <div className="landingServicesTags">
+                <h1>Kitchen cleaning</h1>
+              </div>
+            </div>
 
-      <div className="servicesOfferedExploreBtn">
-        <button className="basicRoundedButton">Explore more</button>
+            <div className="landingServicesBentosThree">
+              <div className="landingServiceBentosImg">
+                <img src="/assets/carwash.png" alt="taprepair" />
+              </div>
+              <div className="landingServicesTags">
+                <h1>Car wash</h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="bentosRowOne">
+            <div className="landingServicesBentos">
+              <div className="landingServiceBentosImg">
+                <img src="/assets/haircutsServiceLanding.png" alt="taprepair" />
+              </div>
+              <div className="landingServicesTags">
+                <h1>Hair cuts</h1>
+              </div>
+            </div>
+
+            <div className="landingServicesBentosFour">
+              <div className="landingServicesTags">
+                <h1>Appliances repair</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bentosExploremoreBtn">
+          <div className="bentosExploreWrapper">
+            <button>
+              Explore more
+              <img src="/assets/upsideArrow.png" alt="explore" />
+            </button>
+          </div>
+
+          <div className="landingServicesBentosFive">
+            <div className="landingServicesTags">
+              <h1>Wall painting</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
